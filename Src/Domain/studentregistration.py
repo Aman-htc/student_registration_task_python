a8ddad1d0884d4f32969d12a4fba459ab6b5eca7
Aman kushwaha <amanhtc1112kushwaha@gmail.com>
2025-04-17 18:13:59 +0530

    student registration project

--- Src/Domain/studentregistration.py
+++ Src/Domain/studentregistration.py
@@ -38,71 +38,71 @@ def report_data():
 
 def details_student():
     data_store= get_load(path)
-    
+    while True:
         
-    
-    while True: 
-        print('1. Press Register and save data in json file!')
+        student_data={}
+        print('*-Start Registration-*')
+        Id=input('please enter your id: ')
+        if Id.isdigit():
+            student_data['id']=int(Id)
+            break
+        else:
+            print()
+            print('invalid!id  enter your digit number only!')    
         
-        print('2. Display Active and Inactive Student!')
-        print('0. Press Exit')
-        choice_number=int(input('please enter your press number: '))
-        if choice_number ==1:
+    while True:
+        name=input('please enter your name: ').lower()
+        
+        if name.isalnum():
+            student_data['name']=name
+            break
+        else:
+            print()
+            print('invalid!name enter the coorect name!') 
             
-        
-            while True:
-                
-                student_data={}
-                print('*-Start Registration-*')
-                Id=input('please enter your id: ')
-                if Id.isdigit():
-                    student_data['id']=int(Id)
-                    break
-                else:
-                    print()
-                    print('invalid!id  enter your digit number only!')    
-                
-            while True:
-                name=input('please enter your name: ').lower()
-                
-                if name.isalnum():
-                    student_data['name']=name
-                    break
-                else:
-                    print()
-                    print('invalid!name enter the coorect name!') 
-                    
-                    
-            while True:
-                contact=input('please enter your contact details: ')        
-                if contact.isdigit() and len(contact)==10:
-                    student_data['contact']=int(contact)
-                    break
-                else:
-                    print()
-                    print('invalid! number enter the correct number!') 
-                    
-                    
-            while True:
-                address=input('please enter your address: ')        
-                if address.isalnum():
-                    student_data['address']=address
-                    break
-                else:
-                    print()
-                    print('invalid! address enter the valid address!') 
-            active=input('is the student an active (yes/no): ').lower()
-            if active=='yes':
-                student_data['active']=True
-            else:
-                student_data['active']=False     
-            data_store.append(student_data) 
-                
-            with open(path,'w') as file:
-                json.dump(data_store,file, indent=4)
-        elif choice_number==2:
             
-            report_data()
-        elif choice_number==0:
-            break          
-details_student()
+    while True:
+        contact=input('please enter your contact details: ')        
+        if contact.isdigit() and len(contact)==10:
+            student_data['contact']=int(contact)
+            break
+        else:
+            print()
+            print('invalid! number enter the correct number!') 
+            
+            
+    while True:
+        address=input('please enter your address: ')        
+        if address.isalnum():
+            student_data['address']=address
+            break
+        else:
+            print()
+            print('invalid! address enter the valid address!') 
+    active=input('is the student an active (yes/no): ').lower()
+    if active=='yes':
+        student_data['active']=True
+    else:
+        student_data['active']=False     
+    data_store.append(student_data) 
+        
+    with open(path,'w') as file:
+        json.dump(data_store,file, indent=4)
+        
+
+def student_menu():
+    print('1. Press Register and save data in json file!')
+        
+    print('2. Display Active and Inactive Student!')
+    print('0. Press Exit')
+    choice_number=int(input('please enter your press number: '))
+    if choice_number ==1:
+        details_student()
+            
+    elif choice_number==2:
+            
+        report_data()
+    elif choice_number==0:
+        break          
+
+
